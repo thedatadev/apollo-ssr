a10b9a605eccc9ea21fc5c8e3c1d0cd69aad2912
Edrian Gomez <thedatadev@gmail.com>
2023-01-11 10:46:43 +1100

    add react views

--- csr/src/App.js
+++ csr/src/App.js
@@ -1,7 +1,72 @@
-import React from "react";
+import * as React from "react";
+import { Link } from "react-router";
 
-function App() {
-  return <h1>React App</h1>;
+import { useQuery, gql } from "@apollo/client";
+
+export default function App(props) {
+  return (
+    <div>
+      <h1>SSR Demo</h1>
+      <ul role="nav">
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/counter">Counter</Link>
+        </li>
+        <li>
+          <Link to="/players">Players</Link>
+        </li>
+      </ul>
+
+      {props.children}
+    </div>
+  );
 }
 
-export default App;
+export function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This page is CLIENT-side rendered!</p>
+    </div>
+  );
+}
+
+export function Counter() {
+  const [clicks, setClicks] = React.useState(0);
+  return (
+    <div>
+      <h1>Clicks: {clicks}</h1>
+      <button onClick={() => setClicks((clicks) => clicks + 1)}>Click</button>
+    </div>
+  );
+}
+
+export const PLAYERS = gql`
+  query getPlayers {
+    players {
+      id
+      name
+    }
+  }
+`;
+
+export function Players() {
+  const { loading, data } = useQuery(PLAYERS);
+
+  if (loading) {
+    return <p>Loading players...</p>;
+  }
+
+  return (
+    <div>
+      <h1>Players</h1>
+      <ul>
+        {data.players.map((player) => (
+          <li key={player.id}>{player.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
--- csr/src/index.js
+++ csr/src/index.js
@@ -1,5 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { Route, Router, browserHistory } from "react-router";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import App, { About, Counter, Players } from "./App";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <Route path="/about" component={About} />
+        <Route path="/counter" component={Counter} />
+        <Route path="/players" component={Players} />
+      </Route>
+    </Router>
+  </ApolloProvider>,
+  document.getElementById("root")
+);
